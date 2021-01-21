c9d4fc9c48af6f4aadae9a233bf5c55c9bc91f8e
dong uk <gusrn1255@naver.com>
2021-01-21 18:25:03 +0900

    연산자 {안에 작성}

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [videos, setVideos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const getData = async () => {
+    return (
+        await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=8597e491ed6e80f0de12e349eb60ea6e')
+            // .then(res => console.log(res.data.results))
+            .then(res => {
+              setVideos(res.data.results)
+              setLoading(false)
+            })
+            .catch(err => console.log(err))
+
+    )
+  }
+
+
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ...
+                    </h1>
+                </div>
+            :    (
+                    <div>
+                        {videos.map(video =>
+                            <>
+                                <h1>Title : {video.original_name || video.original_title}</h1>
+                                <h2>Fisrt Releas Dat : {video.first_air_date || video.release_date}</h2>
+                            </>
+                        )}
+
+                    </div>
+
+                )
+        }
+        </>
+
+    );
+};
 
 export default App;
